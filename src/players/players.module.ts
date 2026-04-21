aa0e49dd05b19379b3187cef40a9987c1ff6d21c
Thu1 <you@example.com>
2026-04-21 18:59:49 +0100

    personajes mergeado en jugadores

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
 import { Player, PlayerSchema } from '../schema/wiki/player.schema';
+import { Character, CharacterSchema } from '../schema/wiki/character.schema';
 import { SharedModule } from '../shared/shared.module';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: Player.name, schema: PlayerSchema }]),
+        MongooseModule.forFeature([
+            { name: Player.name, schema: PlayerSchema },
+            { name: Character.name, schema: CharacterSchema }
+        ]),
         SharedModule
     ],
     controllers: [PlayersController],
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -2,22 +2,48 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Player, PlayerDocument } from '../schema/wiki/player.schema';
+import { Character, CharacterDocument } from '../schema/wiki/character.schema';
 
 @Injectable()
 export class PlayersService {
-    constructor(@InjectModel(Player.name) private playerModel: Model<PlayerDocument>) {}
+    constructor(
+        @InjectModel(Player.name) private playerModel: Model<PlayerDocument>,
+        @InjectModel(Character.name) private characterModel: Model<CharacterDocument>
+    ) {}
 
     async create(data: Partial<Player>): Promise<Player> {
         const created = new this.playerModel(data);
         return created.save();
     }
 
-    async findAll(): Promise<Player[]> {
-        return this.playerModel.find().exec();
+    async findAll(): Promise<any[]> {
+        const players = await this.playerModel.find().lean().exec();
+        const playersWithCharacters = await Promise.all(players.map(async (player) => {
+            const characters = await this.characterModel.find({ playerId: player._id })
+                .populate('subraceId')
+                .populate('currentPlaceId')
+                .populate('deitiesIds')
+                .populate('playerId')
+                .populate('factionId')
+                .lean()
+                .exec();
+            return { ...player, characters };
+        }));
+        return playersWithCharacters;
     }
 
-    async findOne(id: string): Promise<Player> {
-        return this.playerModel.findById(id).exec();
+    async findOne(id: string): Promise<any> {
+        const player = await this.playerModel.findById(id).lean().exec();
+        if (!player) return null;
+        const characters = await this.characterModel.find({ playerId: player._id })
+            .populate('subraceId')
+            .populate('currentPlaceId')
+            .populate('deitiesIds')
+            .populate('playerId')
+            .populate('factionId')
+            .lean()
+            .exec();
+        return { ...player, characters };
     }
 
     async update(id: string, data: Partial<Player>): Promise<Player> {
